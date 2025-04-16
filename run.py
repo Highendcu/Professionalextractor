db3a31071c9f10577020d982c335dfadac97e6e8
Gregory Dill <gregorydill6@gmail.com>
2025-04-15 20:13:27 -0700

    Fix app/socketio return and run config

--- run.py
+++ run.py
@@ -2,5 +2,7 @@ from app import create_app
 
 app, socketio = create_app()
 
-if __name__ == "__main__":
-    socketio.run(app, host="0.0.0.0", port=int(os.environ.get("PORT", 10000)))
+if __name__ == '__main__':
+    import os
+    port = int(os.environ.get('PORT', 10000))
+    socketio.run(app, host='0.0.0.0', port=port)
