7c2f95ddef1be79eb89dca15c2e1c5efc546211a
Gregory Dill <gregorydill6@gmail.com>
2025-04-15 20:10:11 -0700

    Fix indentation error for socketio

--- app/__init__.py
+++ app/__init__.py
@@ -1,19 +1,16 @@
 from flask import Flask
 from flask_cors import CORS
 from flask_socketio import SocketIO
-from .services.extractor import set_socketio
 
-socketio = SocketIO()
+socketio = SocketIO(cors_allowed_origins="*")
 
 def create_app():
     app = Flask(__name__)
+    app.config['SECRET_KEY'] = 'your-secret-key'
     CORS(app)
 
-    app.config['SECRET_KEY'] = 'your-secret-key'
-	
-	socketio.init_app(app)
+    socketio.init_app(app)
 
-    # blueprints here
     from app.routes.admin import admin as admin_blueprint
     from app.routes.api import api as api_blueprint
     from app.routes.main import main as main_blueprint
