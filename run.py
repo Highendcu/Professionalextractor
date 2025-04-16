a31f9e2339a63d38ba1cb05f9e585a759e8fc446
Gregory Dill <gregorydill6@gmail.com>
2025-04-15 20:16:31 -0700

    Allow Werkzeug for deployment

--- run.py
+++ run.py
@@ -5,4 +5,4 @@ app, socketio = create_app()
 if __name__ == '__main__':
     import os
     port = int(os.environ.get('PORT', 10000))
-    socketio.run(app, host='0.0.0.0', port=port)
+    socketio.run(app, host='0.0.0.0', port=port, allow_unsafe_werkzeug=True)
