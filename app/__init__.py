ddcc99254fe8cef8ef7ca8354c37451e3b3fc4b2
Gregory Dill <gregorydill6@gmail.com>
2025-04-15 20:06:48 -0700

    Fix socketio initialization

--- app/__init__.py
+++ app/__init__.py
@@ -3,21 +3,23 @@ from flask_cors import CORS
 from flask_socketio import SocketIO
 from .services.extractor import set_socketio
 
-socketio = SocketIO(cors_allowed_origins="*")
+socketio = SocketIO()
 
 def create_app():
     app = Flask(__name__)
     CORS(app)
 
     app.config['SECRET_KEY'] = 'your-secret-key'
+	
+	socketio.init_app(app)
 
-    # Register your routes
-    from app.routes.main import main as main_blueprint
-    from app.routes.api import api as api_blueprint
+    # blueprints here
     from app.routes.admin import admin as admin_blueprint
+    from app.routes.api import api as api_blueprint
+    from app.routes.main import main as main_blueprint
 
+    app.register_blueprint(admin_blueprint)
+    app.register_blueprint(api_blueprint)
     app.register_blueprint(main_blueprint)
-    app.register_blueprint(api_blueprint, url_prefix='/api')
-    app.register_blueprint(admin_blueprint, url_prefix='/admin')
-    
+
     return app, socketio
--- run.py
+++ run.py
@@ -1,7 +1,6 @@
-import os
 from app import create_app
 
 app, socketio = create_app()
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     socketio.run(app, host="0.0.0.0", port=int(os.environ.get("PORT", 10000)))
